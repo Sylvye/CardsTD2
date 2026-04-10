a59b3f6b0b4aaaa06f3cc49a422ec94f3b5a98d3
Sylvye <152118362+Sylvye@users.noreply.github.com>
2026-04-10 13:02:13 -0400

    Replace multishot spacing with degrees spread

--- Assets/Scripts/Towers/ProjectileTowerAttackDef.cs
+++ Assets/Scripts/Towers/ProjectileTowerAttackDef.cs
@@ -6,10 +6,12 @@ namespace Towers
     public class ProjectileTowerAttackDef : TowerAttackDef
     {
         public TowerProjectile projectilePrefab;
+        [Min(1)] public int projectileCount = 1;
         [Min(0f)] public float damageBonus;
         [Min(0.01f)] public float projectileSpeed = 8f;
         [Min(0.01f)] public float hitRadius = 0.2f;
         [Min(0.01f)] public float projectileLifetime = 4f;
+        [Min(0f)] public float degreesSpread = 0f;
         public Vector3 fireOffset;
         public bool followTarget = false;
 
